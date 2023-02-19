eefc62a2e8944b1bbd45de343bab362b1df414fb
Giana <giana@giana.dev>
2023-02-19 02:06:51 -0600

    Add messages

--- locales/en.lua
+++ locales/en.lua
@@ -1,9 +1,14 @@
 local Translations = {
     error = {
-        not_enough = 'You do not have enough of: %{itemLabel}'
+        not_enough = 'You do not have enough to sell.',
+        cannot_sell = 'You cannot sell right now.'
     },
     info = {
-        police_alert = 'Drug sale in progress'
+        police_alert = 'Drug sale in progress',
+        selling = 'Selling...'
+    },
+    other = {
+        target_label = 'Sell'
     }
 }
 
